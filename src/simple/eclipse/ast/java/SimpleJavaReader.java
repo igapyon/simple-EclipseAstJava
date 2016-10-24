a492bb19be525bbd7fbdf858c16bf4fafc9dcd48
Toshiki Iga <igapyon@gmail.com>
2016-10-24 22:57:23 +0900

    change to apache commons io.

--- src/simple/eclipse/ast/java/SimpleJavaReader.java
+++ src/simple/eclipse/ast/java/SimpleJavaReader.java
@@ -1,30 +1,21 @@
 package simple.eclipse.ast.java;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
 public class SimpleJavaReader {
 	public static void main(final String[] args) throws IOException {
-		final BufferedReader reader = new BufferedReader(new InputStreamReader(
-				new FileInputStream("./src/simple/eclipse/ast/java/SimpleJavaReader.java"), "UTF-8"));
-		final StringBuffer buf = new StringBuffer();
-		for (;;) {
-			String line = reader.readLine();
-			if (line == null)
-				break;
-			buf.append(line);
-		}
-		reader.close();
-
 		final ASTParser parser = ASTParser.newParser(AST.JLS8);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-		parser.setSource(buf.toString().toCharArray());
+		parser.setSource(
+				FileUtils.readFileToString(
+						new File("./src/simple/eclipse/ast/java/SimpleJavaReader.java"), "UTF-8")
+						.toCharArray());
 
 		final CompilationUnit node = (CompilationUnit) parser.createAST(null);
 		final SimpleVisitor visitor = new SimpleVisitor();
